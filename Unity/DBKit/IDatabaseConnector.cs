eba479bd9a2327e520cf872cbeaf089f481506b3
EasyH <skysea001010@gmail.com>
2024-07-04 21:24:11 +0900

    json 읽기 기능 복원

--- Unity/DBKit/IDatabaseConnector.cs
+++ Unity/DBKit/IDatabaseConnector.cs
@@ -1,13 +1,22 @@
 using System;
 using System.Collections.Generic;
 
-public interface IDatabaseConnector<T> {
+namespace EHTool.DBKit {
 
-    public void Connect(string databaseName);
-    public bool IsDatabaseExist();
-    public void AddRecord(T Record);
-    public void GetRecordAt(CallbackMethod<T> callback, CallbackMethod fallback, int idx);
-    public void UpdateRecordAt(T Record, int idx);
-    public void GetAllRecord(CallbackMethod<IList<T>> callback);
+    public interface IDictionaryable<T> {
+        IDictionary<string, object> ToDictionary();
+        static T FromDictionary(IDictionary<string, object> d) => throw new NotImplementedException();
 
+    }
+
+    public interface IDatabaseConnector<T> where T : IDictionaryable<T> {
+
+        public void Connect(string databaseName);
+        public bool IsDatabaseExist();
+        public void AddRecord(T Record);
+        public void GetRecordAt(CallbackMethod<T> callback, CallbackMethod fallback, int idx);
+        public void UpdateRecordAt(T Record, int idx);
+        public void GetAllRecord(CallbackMethod<IList<T>> callback);
+
+    }
 }
